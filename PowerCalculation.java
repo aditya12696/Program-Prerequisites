910c85f60f38ebf1a713e3237f2e1068f2983d53
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 11:49:08 +0530

    Added program to calculate power of a number

---
+++ PowerCalculation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class PowerCalculation{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int b = sc.nextInt();
+		int e = sc.nextInt();
+		double p = Math.pow(b,e);
+		System.out.println(p);
+	}
+}
