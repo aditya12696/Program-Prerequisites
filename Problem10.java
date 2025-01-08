2b966aa1a6127c9635e792bd79fdaa0a5b14ff9e
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 10:40:28 +0530

    Convert Kilometers to Miles

---
+++ Problem10.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Problem7{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double k = sc.nextDouble();
+		double m = k * 0.621371;
+		System.out.println(m);
+	}
+}
