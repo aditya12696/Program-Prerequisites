01097c92d5f4fe6b9037bd005c968654f9942446
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 11:31:49 +0530

    Forcefully remove

--- Problem3.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Problem3{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		double input = sc.nextDouble();
-		double convert = (input * 9/5) +32;
-		System.out.println(convert);
-	}
-}
