2dfc3c72053a9d91f3c7418c319d2377f574ecef
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 11:52:38 +0530

    Added program to convert kilometers to miles

---
+++ ConvertKilometersToMiles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class ConvertKilometersToMiles{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double k = sc.nextDouble();
+		double m = k * 0.621371;
+		System.out.println(m);
+	}
+}
