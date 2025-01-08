64a96a88bcdc595f5001fc655dd9eba2b732286d
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 11:38:18 +0530

    Added program to calculate the area of a circle

---
+++ AreaOfACircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AreaOfACircle{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		double radius = sc.nextDouble();
+		double area = Math.PI * Math.pow(radius,2);
+		System.out.println(area);
+	}
+}
