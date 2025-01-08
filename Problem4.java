b8676ab79c01cb38d3fe5c7aedfe8bb7688b7d3c
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 11:32:33 +0530

    Forcefully remove

--- Problem4.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Problem4{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		double radius = sc.nextDouble();
-		double area = Math.PI * Math.pow(radius,2);
-		System.out.println(area);
-	}
-}
