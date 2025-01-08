beb4edc856d5b1ce8bab28607b2bd540df1bec50
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 11:34:57 +0530

    Forcefully remove

--- Problem10.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Problem7{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		double k = sc.nextDouble();
-		double m = k * 0.621371;
-		System.out.println(m);
-	}
-}
