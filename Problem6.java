4bd9c3a0fae08a73f9180921687371f037f7e330
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 10:19:24 +0530

    Calculate Simple Interest

---
+++ Problem6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Problem6{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int p = sc.nextInt();
+		int t = sc.nextInt();
+		int r = sc.nextInt();
+		float sp = (p*r*t)/100;
+		System.out.println(sp);
+	}
+}
