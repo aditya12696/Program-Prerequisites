842c435ece6ed2cf819cd920e948a8dd1037e601
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-08 10:30:06 +0530

    Power Calculationt

---
+++ Problem8.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Problem7{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int b = sc.nextInt();
+		int e = sc.nextInt();
+		double p = Math.pow(b,e);
+		System.out.println(p);
+	}
+}
